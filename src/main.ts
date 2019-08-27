7e00067351d83aa5ef680bbdc6339430a7eb9b69
Archimedes Trajano <developer@trajano.net>
2019-08-27 15:04:46 -0400

    Changed to point to flow-e

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,1 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './load-cordova';
-import router from './router';
-import store from './store';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+window.location.href = 'https://flow-e.com/workflow';
