4facaefb2ae3d8893d5ba1baf50b721c9cfcd0f8
Archimedes Trajano <archimedes.trajano@devhaus.com>
2019-11-16 09:51:15 -0500

    Added longer test with waits so video can be seen

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,12 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
-  it('Visits the app root url', () => {
+  it('Visits the app root url and goes to about page', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.wait(1000)
+    cy.get('a[href="#/about"]').click()
+    cy.contains('div', 'This is an about page')
+    cy.wait(1000)
   })
 })
