f99f37eab317dd489e56d45f2a9c38da8d674e72
Archimedes Trajano <archimedes.trajano@devhaus.com>
2020-11-18 01:48:04 -0500

    Clean up linting

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,10 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("My First Test", () => {
-  it("Visits the app root url and goes to about page", () => {
+  it("Visits the app root url", () => {
     cy.visit("/");
     cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
-    cy.get('a[href="#/about"]').click();
-    cy.contains("div", "This is an about page");
   });
 });
